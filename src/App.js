125ddca2bea8dadfd7337bace3fce452eb85c96c
namdosikh <muhammad.rasikh@gmail.com>
2025-07-23 11:21:16 +0700

    broken version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
-  return <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>;
+  return (
+    <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
+  );
 }
 
 function App() {
@@ -12,9 +14,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is a broken version of my Docker Image.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
